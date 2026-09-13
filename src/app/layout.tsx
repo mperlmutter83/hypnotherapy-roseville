460806a62f740040d0838b55df68cf53fe66de04
Michael Perlmutter <yescrewnetwork@gmail.com>
2026-09-13 13:38:37 -0700

    Install Google Tag Manager (GTM-PW47TP5L)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,8 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return <html lang="en">
+  return (
+    <html lang="en">
       <head>
         {/* RFM site data - pushed before GTM loads */}
         <script
@@ -30,5 +31,33 @@ window.dataLayer.push({
 });`,
           }}
         />
-      </head><body className={`${inter.variable} font-sans antialiased`}><Header /><main>{children}</main><Footer /></body></html>;
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PW47TP5L');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
+      <body className={`${inter.variable} font-sans antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PW47TP5L"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
 }
